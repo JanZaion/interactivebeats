7583e47f112a4d189e27d6d10be75f1459875448
ZaionCode <zajic815@gmail.com>
2022-05-20 14:42:18 +0200

    add tone transport

--- src/components/Sequencer.js
+++ src/components/Sequencer.js
@@ -1,5 +1,14 @@
 import InstrumentGroup from './InstrumentGroup';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import * as Tone from 'tone';
+
+const ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+Tone.Transport.bpm.value = parseFloat(142);
+
+const topLoop = new Tone.Loop((time) => {
+  console.log(Tone.Transport.position);
+}, '8n').start('1m');
 
 const Sequencer = () => {
   const [play, setPlay] = useState({
@@ -9,16 +18,21 @@ const Sequencer = () => {
     chords: [false, false, false, false],
   });
 
-  const [metro, setMetro] = useState(false);
+  const [transportRunning, setTransportRunning] = useState(false);
+
+  useEffect(() => {
+    transportRunning ? Tone.Transport.start() : (Tone.Transport.stop().position = 0);
+  }, [transportRunning]);
 
   const player = (group, id) => {
+    Tone.start(); //ctx keeps saying suspended for some reason. Hopefuly not a problem
     const updatedGroup = [false, false, false, false];
     updatedGroup[id] = !play[group][id];
     const updatedPlay = { ...play, [group]: updatedGroup };
     setPlay(updatedPlay);
 
     const { drums, bass, chords, melody } = updatedPlay;
-    setMetro([...drums, ...bass, ...chords, ...melody].some((e) => e));
+    setTransportRunning([...drums, ...bass, ...chords, ...melody].some((e) => e));
   };
 
   return (
@@ -31,7 +45,7 @@ const Sequencer = () => {
       {/* clean these when done */}
       <div>{JSON.stringify(play)}</div>
       <br />
-      <div>{`isplaying: ${metro}`}</div>
+      <div>{`isplaying: ${transportRunning}`}</div>
     </main>
   );
 };
