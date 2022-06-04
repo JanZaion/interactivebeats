1b64521bd70b5c656dbd8cca08b1010ee94c2001
ZaionCode <zajic815@gmail.com>
2022-06-04 12:16:57 +0200

    refac anim logic to switch statement

--- src/components/Pad.js
+++ src/components/Pad.js
@@ -5,13 +5,20 @@ const pickAnimation = (activePad, queuedPad, group, groupParams) => {
   const measureInSeconds = Time(Transport.seconds === 0 ? 0 : groupParams[group].measure).toSeconds();
   const transportPositionRemainder = Transport.seconds % measureInSeconds;
   const animationSeconds = measureInSeconds - transportPositionRemainder;
+  const padStates = `${JSON.stringify(activePad)} ${JSON.stringify(queuedPad)}`;
 
-  if (activePad === false && queuedPad === false) return { animation: ``, background: `` };
-  if (activePad === true && queuedPad === true) return { animation: ``, background: `${groupParams[group].color}` };
-  if (activePad === true && queuedPad === false)
-    return { animation: `${group}StartAnim ${animationSeconds}s`, background: `${groupParams[group].color}` };
-  if (activePad === false && queuedPad === true)
-    return { animation: `${group}StopAnim ${animationSeconds}s`, background: `` };
+  switch (padStates) {
+    case 'false false':
+      return { animation: ``, background: `` };
+    case 'true true':
+      return { animation: ``, background: `${groupParams[group].color}` };
+    case 'true false':
+      return { animation: `${group}StartAnim ${animationSeconds}s`, background: `${groupParams[group].color}` };
+    case 'false true':
+      return { animation: `${group}StopAnim ${animationSeconds}s`, background: `` };
+    default:
+      return;
+  }
 };
 
 const Pad = ({ handlePadClick, id, group, activePad, queuedPad }) => {
