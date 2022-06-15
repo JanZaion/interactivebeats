128791d2bd245d2762082167adc434cac5b0de2e
ZaionCode <zajic815@gmail.com>
2022-06-15 12:22:47 +0200

    add metastuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Sequencer from './components/Sequencer';
 import Intro from './components/Intro';
+import Outro from './components/Outro';
 import { groupParams } from './constants/fixedParams';
 
 //single source of truth for pad colors, once tracks are loadable, put it somwhere where they are being loaded
@@ -15,6 +16,7 @@ function App() {
       <main className="appContainer">
         <Intro />
         <Sequencer />
+        <Outro />
       </main>
     </>
   );
---
+++ src/components/Outro.js
@@ -0,0 +1,12 @@
+const Outro = () => {
+  return (
+    <p className="intro">
+      If you like this project, consider starring it on&nbsp;
+      <a href="https://github.com/JanZaion/bossequencer" target="about:blank">
+        Github
+      </a>
+    </p>
+  );
+};
+
+export default Outro;
