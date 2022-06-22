3e5b4ecc279dfb44aec8105b31019bf09f5ea987
ZaionCode <zajic815@gmail.com>
2022-06-22 20:19:35 +0200

    creat a wrapper for the sequencer

---
+++ src/components/Wrapper.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import Sequencer from './components/Sequencer';
+
+const Wrapper = () => {
+  return (
+    <>
+      <Sequencer />
+    </>
+  );
+};
+
+export default Wrapper;
