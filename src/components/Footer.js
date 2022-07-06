f379b031680aa8f97f1b8e0916586d7098b3d856
ZaionCode <zajic815@gmail.com>
2022-07-06 14:37:01 +0200

    remove footer divider

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,6 @@
 const Footer = () => {
-  console.log(window.innerHeight);
   return (
-    <footer className="contentBox">
-      <div className="footerDivider" />
+    <footer className="contentBox" id="footer">
       Developed by Jan Zaion. If you like this project, consider starring it on&nbsp;
       <a href="https://github.com/JanZaion/bossequencer" target="about:blank" className="githubLink">
         Github
