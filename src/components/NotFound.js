0f70cbfb4c8d7a30eaecfee93ad46a95a30dcf24
ZaionCode <zajic815@gmail.com>
2022-07-07 10:46:55 +0200

    split loading and notfound ids

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,5 +1,5 @@
 const NotFound = ({ route }) => {
-  return <p className="contentBox" id="loading">{`No track named "${route}" found, sorry :'(`}</p>;
+  return <p className="contentBox" id="notFound">{`No track named "${route}" found, sorry :'(`}</p>;
 };
 
 export default NotFound;
