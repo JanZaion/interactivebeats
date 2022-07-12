e830a1add71f3d8087685fa1d2121f69413d468b
ZaionCode <zajic815@gmail.com>
2022-07-12 15:12:38 +0200

    make fftviz cool af

--- src/components/FFTviz.js
+++ src/components/FFTviz.js
@@ -1,7 +1,13 @@
-import { useState, useRef, useEffect } from 'react';
-import { FFT, Analyser } from 'tone';
+import { useRef, useEffect } from 'react';
+import { FFT, Transport } from 'tone';
 
-const fft = new FFT(16);
+const FFTcenter = 80;
+const initialxOffset = 3;
+const FFTlinesWidth = 2;
+const FFTdensity = 16 * 2;
+const fft = new FFT(FFTdensity);
+const upperBound = 110;
+const lowerBound = 70;
 
 const FFTviz = ({ players }) => {
   players.forEach((player) => player.fan(fft));
@@ -9,69 +15,58 @@ const FFTviz = ({ players }) => {
   const ctxRef = useRef(null);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    ctxRef.current = ctx;
-    draw();
-  }, []);
+    ctxRef.current = canvasRef.current.getContext('2d');
+    idleFFT();
+  }, []); //eslint-disable-line
 
-  const draw = () => {
-    requestAnimationFrame(draw);
+  useEffect(() => {
+    Transport.state === 'stopped' && drawFFT();
+  }, [Transport.state]); //eslint-disable-line
+
+  const calculateXoffset = () => (canvasRef.current.offsetWidth + canvasRef.current.offsetWidth / 2) / FFTdensity;
+
+  const idleFFT = () => {
+    const ctxr = ctxRef.current;
+
+    let xOffset = initialxOffset;
+    for (let i = 0; i < FFTdensity; i++) {
+      ctxr.lineWidth = FFTlinesWidth;
+      ctxr.beginPath();
+      ctxr.strokeStyle = 'white';
+      ctxr.moveTo(xOffset, FFTcenter);
+      ctxr.lineTo(xOffset, upperBound);
+      ctxr.lineTo(xOffset, lowerBound);
+      ctxr.stroke();
+      xOffset += calculateXoffset();
+    }
+  };
+
+  const drawFFT = () => {
+    requestAnimationFrame(drawFFT);
     const fftVals = fft.getValue();
     const ctxr = ctxRef.current;
 
-    ctxr.clearRect(0, 0, 500, 500);
+    ctxr.clearRect(0, 0, 1000, 1000);
 
-    const dots = [];
-    // const dots2 = [];
-    for (let i = 0; i < 6; i++) dots.push(fftVals[i]);
-    // for (let i = 8; i > 0; i--) dots2.push(fftVals[i]);
-    // const dots2 = [...dots];
-    // dots2.reverse();
-    // const dotsFinal = dots2.concat(dots);
-    const dots2 = [...dots].reverse();
-    const dotsFinal = [];
-    for (let i = 0; i < 6; i++) {
-      dotsFinal.push(dots[i]);
-      dotsFinal.push(dots2[i]);
-    }
+    const lines = [];
+    for (let i = FFTdensity / 8; i < FFTdensity; i++) lines.push(fftVals[i]);
 
-    const c = () => canvasRef.current.getBoundingClientRect().width / 16;
-    let p = c();
-    const connections = 12;
-    for (let i = 0; i < connections; i++) {
-      let prep = p;
-      // i % 2 === 0 ? (ctxr.fillStyle = 'green') : (ctxr.fillStyle = 'purple');
-      // const up = Math.log(fftVals[i] * -1) * 20;
-      // const up = i%2===0?
-      // ctxr.fillRect(0, fftVals[i] * -1, p, 500);
-      ctxr.lineWidth = 2;
+    let xOffset = initialxOffset;
+    for (let i = 0; i < FFTdensity; i++) {
+      ctxr.lineWidth = FFTlinesWidth;
       ctxr.beginPath();
       ctxr.strokeStyle = 'white';
-      ctxr.moveTo(p, 80);
-      if (i + 1 !== connections) {
-        ctxr.lineTo(prep + c() / 2, dotsFinal[i] * -1);
-        ctxr.lineTo(prep + c(), 80);
-      }
+      ctxr.moveTo(xOffset, FFTcenter);
+      const fftVal1 = lines[i] * -1;
+      ctxr.lineTo(xOffset, fftVal1 > upperBound ? upperBound : fftVal1);
+      const fftVal2 = lines[i] + 160;
+      ctxr.lineTo(xOffset, fftVal2 < lowerBound ? lowerBound : fftVal2);
       ctxr.stroke();
-      p += c();
+      xOffset += calculateXoffset();
     }
   };
 
-  const click = () => {
-    const fftVals = fft.getValue();
-    // console.log(canvasRef.current.getBoundingClientRect().width);
-    console.log(fftVals);
-
-    // draw();
-  };
-
-  return (
-    <div className="contentBox">
-      {/* <button onClick={() => click()}>click</button> */}
-      <canvas ref={canvasRef} className="fft" />
-    </div>
-  );
+  return <div className="contentBox">{<canvas ref={canvasRef} className="fft" />}</div>;
 };
 
 export default FFTviz;
