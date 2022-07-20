c08936f585d716b0205e97af59af2fc24018bda7
ZaionCode <zajic815@gmail.com>
2022-07-20 13:42:16 +0200

    fix refresh 404

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
+    <BrowserRouter bassname="/bossequencer">
       <main className="appContainer">
         <FFTviz players={players} />
         <TrackSelector players={players} />
